a556680a4faef0743667b4d34a57a6a6bdfd0085
lilux91 <lilualtamiran@gmail.com>
2022-10-19 17:06:10 -0300

    termino SelectByType

---
+++ src/components/home/pokedex/SelectByType.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import React from 'react'
+
+const SelectByType = ({setTypesSelected}) => {
+
+  const [types, setTypes] = useState()
+
+
+
+ useEffect(() => {
+  const URL ='https://pokeapi.com.api/v2/type'
+ axios.get(URL)
+    .then(res => setTypes(res.data))
+    .catch(err => console.log(err))
+
+ }, [])
+ 
+ //console.log(types)
+  const handleChange = e => {
+    setTypesSelected(e.target.value)
+  }
+
+  return (
+    <select onChange={handleChange}>
+        <option value='All Pokemons'>All Pokemons</option>
+        {
+          types?.map(type => (
+            <option key={type.url} value={type.url}>{type.name}</option>
+          ))
+        }
+    </select>
+  )
+}
+
+export default SelectByType
--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -2,18 +2,36 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import InputSearch from '../components/home/pokedex/InputSearch'
+import SelectByType from '../components/home/pokedex/SelectByType'
 import CardPoke from '../components/home/pokedex/styles/CardPoke'
 
 const Pokedex = () => {
 
   const [pokemons, setPokemons] = useState()
+  const [typeSelected, setTypeSelected] = useState('All Pokemons')
 
 useEffect(() => {
+
+  if (typeSelected !== 'All Pokemons'){
+
+
+    //Si se selecciono un tipo
+    axios.get(typeSelected)
+     .then(res => {
+      const result = res.data.pokemon.map(e => e.pokemon)
+      setPokemons(result)
+     })
+     .catch(err => console.log(err))
+  } else{
+
+
+    //Si quiero todos los pokemons
   const URL = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
   axios.get(URL)
-  .then(res => setPokemons(res.data.results))
-  .catch(err => console.log(err))
-}, [])
+  .then(res => setPokemons= (res.data.results))
+    .catch(err => console.log(err))
+  }
+}, [typeSelected])
 
 //console.log(pokemons)
 
@@ -30,7 +48,7 @@ const initialPoke =(page - 1) * pokePerPage
                //initialPoke + pokePerPage +1
 const finalPoke = page * pokePerPage
 
-}
+
 
   return (
     <div>
@@ -40,7 +58,7 @@ const finalPoke = page * pokePerPage
       </header>
     <aside>
       <InputSearch/>
-      <SelectByType setTypeSelected={setTypeSelect} />
+      <SelectByType setTypeSelected={setTypeSelected}/> 
     </aside>
     <main>
       <div className='card-container'>
@@ -58,6 +76,6 @@ const finalPoke = page * pokePerPage
     </div>
   )
 
-
+}    
 
 export default Pokedex
