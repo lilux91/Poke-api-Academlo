d7361a5520b9651429ddbf16f173ada99d5d0a55
annydrn <andrea.duran.cabana@gmail.com>
2022-10-24 18:17:37 -0300

    creacion de css 404 error y actualizacion de pagination css

--- src/components/home/pokedexId/Pokemon404.jsx
+++ src/components/home/pokedexId/Pokemon404.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../pokedexId/style/pokemon404.css'
+
 
 const Pokemon404 = () => {
   return (
-    <>
+    <section className='container_error'>
      <h1>Pokemon not Found 😖</h1>
      <Link to= '/pokedex'>Return to Pokedex </Link>
-    </>
+ </section>
 
 
   )
